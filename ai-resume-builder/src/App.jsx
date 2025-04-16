c9584a4a2a1619c7bb6115634546c06227f759cd
Imam Bahri Alwi <115518832+alwi2022@users.noreply.github.com>
2025-04-16 18:29:47 +0700

    feat: setup project

---
+++ ai-resume-builder/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div>GatauAi</div>
+      <Button>Wassap</Button>
+    </>
+  );
+}
+
+export default App;
